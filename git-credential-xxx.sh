2c4a4b844913a78ad2b623590d1e3ae1447b9be9
Colin <colin1124x@gmail.com>
2018-06-29 17:54:47 +0800

    .

--- git-credential-xxx.sh
+++ git-credential-xxx.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-
+# this is just a sample demo
+# do not use in production envirenment
 set -x
+
+store=/tmp/x.sotre
+
+while getopts hf: opt
+do
+    case $opt
+    in
+    f) 
+        if [ -n "$store" ]
+        then
+            store=$OPTARG
+        fi
+        shift 2
+        ;;
+    h)
+        echo Usage git config credential.[your.git.domain].helper 'xxx.sh -f [STORE FILE]'
+        shift
+        ;;
+    esac
+
+done
 
 case ${1}
 in
@@ -9,10 +31,10 @@ in
         do
             echo ++ $line >&2
         done
-        cat /tmp/x.store
+        cat $store
         ;;
     store)
-        cat - > /tmp/x.store 
+        cat - > $store
         ;;
     erase)
         ;;
